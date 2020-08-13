abae56ec65fa416a36fbe68307d83aca9e99a369
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-08-13 10:48:45 +0900

    package.json Update

--- webCrawling.js
+++ webCrawling.js
@@ -1,71 +1,26 @@
-// const axios = require("axios");
-// const cheerio = require("cheerio");
-// const log = console.log;
-//
-//
-// const getHtml = async() => {
-//     try {
-//         return await axios.get("http://127.0.0.1:2000");
-//         //return await axios.get("https://news.naver.com/");
-//     } catch (error) {
-//         console.error(error);
-//     }
-// };
-//
-// // getHtml : axios.get 함수를 이용하여 비동기로 html 파일을 가져옴
-// // 그 후 반환되는 Promise 객체에 cheerio 를 이용하여 데이터를 가공함
-// getHtml()
-//     .then(html => {
-//         let ulList = [];
-//         const $ = cheerio.load(html.data);
-//         const $bodyList = $("div.wrap-container").children("div.container");
-//         // const $bodyList = $("div.section_wide ul").children("div.right.ranking_contents");
-//         $bodyList.each(function(i, elem) {
-//             ulList[i] = {
-//                 title: $(this).find('header.mainError p').text(),
-//                 url: $(this).find('herder.mainError p').attr('Errorname'),
-//                 // image_url: $(this).find('p.poto a img').attr('src'),
-//                 // image_alt: $(this).find('p.poto a img').attr('alt'),
-//                 // summary: $(this).find('p.lead').text().slice(0, -11),
-//                 // date: $(this).find('span.p-time').text()
-//             };
-//         });
-//         const data = ulList.filter(n => n.title);
-//         return data;
-//     })
-//     .then(res => log(res));
-//
-//
-var express = require('express');
-var router = express.Router();
 const axios = require("axios");
-//읽어온 정보를 html로직으로 변환하여 화면작업을 하도록 유도합니다
-const cheerio = require('cheerio');
-
-router.get('/', function(req, res, next) {
-    let url = 'https://www.yna.co.kr/sports/all';
-
-    axios.get(url).then(html => {
+const cheerio = require("cheerio");
+const log = console.log;
+const getHtml = async () => {
+    try { return await axios.get("https://www.naver.com/");
+    // axios.get 함수를 이용하여 비동기로 네이버의 html 파일을 가져온다.
+    } catch (error) {
+        console.error(error);
+    }
+};
+getHtml()
+    .then(html => {
         let ulList = [];
         const $ = cheerio.load(html.data);
-        const $bodyList = $("div.headline-list ul").children("li.section02");
-        //each : list 마다 함수 실행, forEach와 동일
+        const $bodyList = $("div.realtime_area ul.list_realtime")
+            .children("li.realtime_item");
+
         $bodyList.each(function(i, elem) {
             ulList[i] = {
-                //find : 해당 태그가 있으면 그 요소 반환
-                title: $(this).find('strong.news-tl a').text(),
-                url: $(this).find('strong.news-tl a').attr('href'),
-                image_url: $(this).find('p.poto a img').attr('src'),
-                image_alt: $(this).find('p.poto a img').attr('alt'),
-                summary: $(this).find('p.lead').text().slice(0, -11),
-                date: $(this).find('span.p-time').text()
+                title: $(this).find('span.keyword').text(),
+                url: $(this).find('a.link_keyword').attr('href')
             };
         });
-
         const data = ulList.filter(n => n.title);
-        //json으로 변환하여 app으로 전송
-        return res.json(data);
-    })
-});
-
-module.exports = router;
+        return data;
+    }) .then(res => log(res));
